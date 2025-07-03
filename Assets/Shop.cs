5d53ae8692ccfd5ef446a83fdb231a304f803a1f
ArTeGaS-2 <probablytorvik@gmail.com>
2025-07-03 19:58:38 +0300

    Методи кнопки магазину

--- Assets/Shop.cs
+++ Assets/Shop.cs
@@ -9,13 +9,33 @@ public class Shop : MonoBehaviour
 
     // GameObject - будь який об'єкт на сцені.
     public GameObject shopWindow; // Вікно магазину
+    public GameObject gameWindow; // Вікно гри 
     private void Start()
     {
         // SetActive - активує чи деактивує об'єкт на сцені
         shopWindow.SetActive(shopWindowStatus); // Декактивуємо 
+        gameWindow.SetActive(!shopWindowStatus); // Активуємо
+
     }
     public void ActivateShop()
     {
-
+        shopWindowStatus = true; // Змінюємо стан індикатора
+    }
+    public void DeactivateShop()
+    {
+        shopWindowStatus = false;
+    }
+    private void Update()
+    {
+        if (shopWindowStatus == true) // Якщо магазин активний
+        {
+            shopWindow.SetActive(true); // вмикаємо вікно магазину
+            gameWindow.SetActive(false); // вимикаємо вікно гри
+        }
+        else if (shopWindowStatus == false) // Зворотня перевірка
+        {
+            shopWindow.SetActive(false);
+            gameWindow.SetActive(true);
+        }
     }
 }
